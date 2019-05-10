84ddf84e3702cec4cc9d1b3ab75fe6bfcb96dbfa
RupanMayan <50477886+RupanMayan@users.noreply.github.com>
2019-05-10 12:54:30 +0530

    Sample1.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,6 @@ class Sample{
   public static void main(String[] arguments) {
     for(int i=0;i<5;i++)
     System.out.println("Hello rupan1");
-System.out.println("Hello jda1");
+System.out.println("Hello rupan");
   }
 }
